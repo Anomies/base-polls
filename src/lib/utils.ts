40d69c114869e0706cefe17654909615b876a828
Anomi <herculestr32@gmail.com>
2025-12-16 19:50:50 +0300

    image paths adjusted

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,9 @@ import { twMerge } from "tailwind-merge"
 export const METADATA = {
   name: "Base Polls",
   description: "Daily Base Polls, Surveys, and Votes on-chain and on Farcaster",
-  bannerImageUrl: 'https://i.imgur.com/2bsV8mV.png',
-  iconImageUrl: 'https://i.imgur.com/brcnijg.png',
+  imageUrl: ".../public/opengraph-image.png",
+  bannerImageUrl: '.../public/opengraph-image.png',
+  iconImageUrl: '.../public/icon.png',
   homeUrl: process.env.NEXT_PUBLIC_URL ?? "https://base-polls.vercel.app/",
   splashBackgroundColor: "#FFFFFF",
   primaryCategory: ["primaryCategories.social"]
