6758decb929dc999ed275889589de999c88af1a9
Anomi <herculestr32@gmail.com>
2025-12-16 20:11:34 +0300

    Account association established

--- src/components/actions/set-primary-button.tsx
+++ src/components/actions/set-primary-button.tsx
@@ -1,25 +1,25 @@
-// "use client";
+"use client";
 
-// import { useCallback } from "react";
-// import { sdk } from "@farcaster/miniapp-sdk";
-// import { Button } from "~/components/ui/Button";
+import { useCallback } from "react";
+import { sdk } from "@farcaster/miniapp-sdk";
+import { Button } from "~/components/ui/Button";
 
-// export function SetPrimaryButtonAction() {
-//   const setPrimaryButton = useCallback((): void => {
-//     sdk.actions.setPrimaryButton({
-//       text: "New label set!!",
-//       loading: false,
-//       disabled: false,
-//       hidden: false,
-//     });
-//   }, []);
+export function SetPrimaryButtonAction() {
+  const setPrimaryButton = useCallback((): void => {
+    sdk.actions.setPrimaryButton({
+      text: "New label set!!",
+      loading: false,
+      disabled: false,
+      hidden: false,
+    });
+  }, []);
 
-//   return (
-//     <div className="mb-4">
-//       <div className="p-2 bg-gray-100 dark:bg-gray-800 rounded-lg my-2">
-//         <pre className="font-mono text-xs text-emerald-500 dark:text-emerald-400">sdk.actions.setPrimaryButton</pre>
-//       </div>
-//       <Button onClick={setPrimaryButton}>Set Primary Button</Button>
-//     </div>
-//   );
-// } 
+  return (
+    <div className="mb-4">
+      <div className="p-2 bg-gray-100 dark:bg-gray-800 rounded-lg my-2">
+        <pre className="font-mono text-xs text-emerald-500 dark:text-emerald-400">sdk.actions.setPrimaryButton</pre>
+      </div>
+      <Button onClick={setPrimaryButton}>Set Primary Button</Button>
+    </div>
+  );
+} 
