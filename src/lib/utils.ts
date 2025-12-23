dc114e8b439d7c93a3b2c8d2d4aaaff3bf4bb8b5
Anomi <herculestr32@gmail.com>
2025-12-23 21:14:38 +0300

    Image changed.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,8 @@ export const METADATA = {
   name: "Base Polls",
   description: "Daily Base Polls, Surveys, and Votes on-chain and on Farcaster",
   imageUrl: 'https://imgur.com/a/KselwNG.png',
-  bannerImageUrl: 'https://imgur.com/vvFlKW7.png',
-  iconImageUrl: 'https://imgur.com/vvFlKW7.png',
+  bannerImageUrl: 'https://imgur.com/a/gNFQ4Zj',
+  iconImageUrl: 'https://imgur.com/a/gNFQ4Zj',
   homeUrl: process.env.NEXT_PUBLIC_URL ?? "https://base-polls.vercel.app",
   splashBackgroundColor: "#FFFFFF",
   primaryCategory: ["primaryCategories.social"]
